501c5a57a6e12b31c57c7c6ab81f87ad5c8ca3e7
gustavo <gustavo.isaac@gmail.com>
2021-03-30 10:15:43 +0100

    Method to start google play

--- app/src/main/java/com/artemissoftware/cadmus/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/cadmus/MainActivity.kt
@@ -12,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startHuaweiAppGallery("102312221");
+        //--startHuaweiAppGallery("102312221");
+        startGooglePlay("com.dts.freefireth")
     }
 
 
@@ -38,6 +39,33 @@ class MainActivity : AppCompatActivity() {
 
         if (!agFound) {
             intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://appgallery.cloud.huawei.com/marketshare/app/C$appHuaweiId"))
+            startActivity(intent)
+        }
+    }
+
+
+    private fun startGooglePlay(appPackageName: String) {
+
+        var intent = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName"))
+
+        val otherApps = packageManager.queryIntentActivities(intent, 0)
+        var psFound = false
+
+        for (app in otherApps) {
+            if (app.activityInfo.applicationInfo.packageName == "com.android.vending") {
+                val psComponent = ComponentName(
+                    app.activityInfo.applicationInfo.packageName,
+                    app.activityInfo.name
+                )
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                intent.component = psComponent
+                startActivity(intent)
+                psFound = true
+                break
+            }
+        }
+        if (!psFound) {
+            intent = Intent(Intent.ACTION_VIEW, Uri.parse(/*"https://play.google.com/store/apps/details?id=$appPackageName"*/"https://play.google.com/store/apps/details?id=cgd.pt.caixadirectaparticulares&amp;hl=pt_PT"))
             startActivity(intent)
         }
     }
